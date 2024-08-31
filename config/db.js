2a04457fcebc506dedcf147debbee85c16ffd6af
jazeem <jazeemmp404@gmail.com>
2024-08-31 13:56:30 +0530

    fix

--- config/db.js
+++ config/db.js
@@ -1,14 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    // Connect to MongoDB
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error.message);
+    console.error(`Error: ${error.message}`);
     process.exit(1);
   }
 };
