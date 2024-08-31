a7fb99c52160f5d82b4840b755fa55fd3e62fce4
jazeem <jazeemmp404@gmail.com>
2024-08-31 13:32:35 +0530

    Fix MongoDB connection code

--- config/db.js
+++ config/db.js
@@ -1,12 +1,14 @@
-const mongose = require("mongoose");
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
-    console.log(`MongoDB Connected:`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log(error);
+    console.error('Error connecting to MongoDB:', error.message);
     process.exit(1);
   }
 };
